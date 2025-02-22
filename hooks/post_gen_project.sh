137a9dbdf787912732e07034fb060c9b4f695e95
Robert Myers <rmyers@anaconda.com>
2025-02-22 14:43:17 -0600

    Use make more

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -3,5 +3,8 @@
 make setup
 make generate args=--force
 
-echo -n "\n\n{{cookiecutter.project_slug}} Setup Successfully!"
-echo -n "Commands:\n\trun: make run\n\thelp: make"
+echo ""
+echo "{{cookiecutter.project_slug}} Setup Successfully!"
+echo "Get started:"
+echo ""
+make
